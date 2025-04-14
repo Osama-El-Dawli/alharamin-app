4490473738434e5e660c4d375886d6d1bdd34e40
osama <osamasaeed283@gmail.com>
2025-04-15 01:25:14 +0200

    add splash screen

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsAndroid12Logo
+  /// assets/android12_logo.png
+  static const String assetsAndroid12Logo = "assets/android12_logo.png";
+
+  /// Assets for assetsImagesLogo
+  /// assets/images/logo.png
+  static const String assetsImagesLogo = "assets/images/logo.png";
+
+  /// Assets for assetsImagesSplash
+  /// assets/images/splash.png
+  static const String assetsImagesSplash = "assets/images/splash.png";
+
+  /// Assets for assetsSplash
+  /// assets/splash.png
+  static const String assetsSplash = "assets/splash.png";
+}
+
--- lib/core/routes/router.dart
+++ lib/core/routes/router.dart
@@ -2,5 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(
-  routes: [GoRoute(path: '/', builder: (context, state) => const Scaffold())],
+  routes: [
+    GoRoute(
+      path: '/',
+      builder:
+          (context, state) => const Scaffold(
+            body: Center(
+              child: Text(
+                'Hello Guys !',
+                style: TextStyle(color: Color(0xff8C822D), fontSize: 30),
+              ),
+            ),
+          ),
+    ),
+  ],
 );
