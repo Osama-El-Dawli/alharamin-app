1ad045c61ea9e8370af7dba48fa7a4bf85fd0e64
Osama El-Dawli <osamasaeed283@gmail.com>
2025-04-14 16:01:34 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/alharamin_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.alharamin_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:alharamin_app/core/routes/router.dart';
+import 'package:alharamin_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const AlharaminApp());
+}
+
+class AlharaminApp extends StatelessWidget {
+  const AlharaminApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: router,
+    );
+  }
+}
